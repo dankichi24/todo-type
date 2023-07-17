c1da2b43f17a6abc067bfc5f22f5b2c0411249ab
dankichi24 <dan24.yukito@gmail.com>
2023-07-18 01:45:43 +0900

    🏷️ フォームに入力された文字列を状態 を保持

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 export const App = () => {
+  const [text, setText] = useState("");
+
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
         <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
+        <input type="submit" />
       </form>
+
+      {/* ↓ DOM のリアクティブな反応を見るためのサンプル */}
+      <p>{text}</p>
+      {/* ↑ あとで削除 */}
     </div>
   );
 };
