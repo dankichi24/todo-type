7d419e0009e1f233012ee63f1223ae5a57cfc31b
dankichi24 <dan24.yukito@gmail.com>
2023-07-18 02:33:17 +0900

    🏷️ イベントを処理する関数を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import { useState } from "react";
 
+type Todo = {
+  value: string;
+};
+
 export const App = () => {
   const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+    };
+
+    setTodos((todos) => [newTodo, ...todos]);
+    setText("");
+  };
 
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input type="submit" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={(e) => handleChange(e)} />
+        <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
-
-      {/* ↓ DOM のリアクティブな反応を見るためのサンプル */}
-      <p>{text}</p>
-      {/* ↑ あとで削除 */}
     </div>
   );
 };
